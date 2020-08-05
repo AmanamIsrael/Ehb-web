25722c3be9f77b86d0a620e540cad6aa0e09325d
Amanam Israel <amanam42@hotmail.com>
2020-08-05 11:45:16 +0100

    added components and auth services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReadComponent } from './components/read/read.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { LibraryComponent } from './components/library/library.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReadComponent,
+    SettingsComponent,
+    ProfileComponent,
+    LibraryComponent
   ],
   imports: [
     BrowserModule,
