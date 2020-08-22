849b2289c809f68e355c50ef3ec4a11ebbba0ce0
Amanam Israel <amanam42@hotmail.com>
2020-08-22 15:50:38 +0100

    added blank component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { ReadComponent } from './components/read/read.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { LibraryComponent } from './components/library/library.component';
 import { SettingsComponent } from './components/settings/settings.component';
+import { BlankComponent } from './components/blank/blank.component';
 
 const routes: Routes = [
-  {path: '', component: ReadComponent, children: [
+    {path: '', component: BlankComponent, children: [
+    {path: 'read', component: ReadComponent},
     {path: 'profile', component: ProfileComponent},
     {path: 'library', component: LibraryComponent},
     {path: 'settings', component: SettingsComponent}
-  ]},
+  ]}
 ];
 
 @NgModule({
